bf7ee20b370d49f80ef6bb5ee2ef12570a03f964
lostrennie <lostrennie@gmail.com>
2016-04-18 10:10:03 -0400

    updated controller with grantlink

--- app/controllers/sekret/people_controller.rb
+++ app/controllers/sekret/people_controller.rb
@@ -58,6 +58,6 @@ class Sekret::PeopleController < SekretController
 		params.require(:person).permit(
 			:fname, :lname, :user_name, :auth_id, :school, :building, 
 			:office, :phone, :pi1, :pi2, :boss_id, :interest, :research, :search, :lab_name,
-			:position, :title, :prefix, :focus1, :focus2, :focus3, :labweb)
+			:position, :title, :prefix, :focus1, :focus2, :focus3, :labweb, :grantlink)
 	end
 end
