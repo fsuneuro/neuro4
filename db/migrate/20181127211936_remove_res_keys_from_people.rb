36ba57ff75727e2d98160a820ee90443560ee26b
Jason <orman@neuro.fsu.edu>
2018-11-29 17:53:51 -0500

    Revert "migration for db field removal reskeys"
    This reverts commit fb3e732984fd9a2255f62b0a6e3c3e15a3aced33.

--- db/migrate/20181127211936_remove_res_keys_from_people.rb
+++
@@ -1,9 +0,0 @@
-class RemoveResKeysFromPeople < ActiveRecord::Migration
-  def change
-  	remove_column :people, :reskey1
-  	remove_column :people, :reskey2
-  	remove_column :people, :reskey3
-  	remove_column :people, :startdate
-  	remove_column :people, :graddate
-  end
-end
